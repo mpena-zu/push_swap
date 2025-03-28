3e76c976294f5719f78a074892b9f8885d816534
Mauricio <aresmauricio33@gmail.com>
2025-03-28 14:35:00 +0100

    Push Swap En Progreso

--- push_swap.c
+++ push_swap.c
@@ -11,8 +11,43 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libft/ft_atoi.c"
 
-int main(int argc, char *argv)
+void	push(t_stack **stack, int value)
 {
-    
+	t_stack *new_node;
+
+	new_node = (t_stack *)malloc(sizeof(t_stack));
+	if (!new_node)
+		return;
+	new_node->value = value;
+    new_node->next = *stack;
+    *stack = new_node;
+}
+int main(int argc, char **argv)
+{
+	int			value;
+	int			i;
+	t_stack		*stack;
+
+	stack = NULL;
+	i = 1;
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	while (i < argc)
+	{
+		value = ft_atoi(argv[i]);
+		if(value == 0)
+		{
+			printf("Error, letra\n");
+			return (1);
+		}
+		push(&stack, value);
+		printf("El valor del argumento del stack es: %d\n", value);
+		i++;
+	}
+	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -12,4 +12,12 @@
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include <stdio.h>
+
+typedef struct s_stack
+{
+    int             value;
+    struct  s_stack *next;    
+}   t_stack;
+
 #endif
